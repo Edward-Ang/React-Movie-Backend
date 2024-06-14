48597d2157403772afd8c4d76d244a517bde4791
Edward Ang <wei.hen.ang@gmail.com>
2024-06-14 17:13:22 +0800

    chg schema  and bcrypt

--- models/favourite.js
+++ models/favourite.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 // Define the Favourite schema
 const favouriteSchema = new mongoose.Schema({
-    movieId: { type: String, required: true, unique: true },
+    movieId: { type: String, required: true },
     movieName: { type: String, required: true },
     movieDate: { type: Date, required: true },
     rating: { type: Number, required: true, min: 0, max: 10 },
