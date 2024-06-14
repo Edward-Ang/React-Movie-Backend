4960505ed57c83214bd9b6869e0f2bcb0c94edf7
Edward Ang <wei.hen.ang@gmail.com>
2024-06-14 17:09:05 +0800

    chg chema

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const passportStrategy = require("./passport");
 const app = express();
 
 // Connect to MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/PopWatch')
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
